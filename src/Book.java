f3a6748906b621b6745ca15cf25a142b7808161a
jazzie-b <33510723+jazzie-b@users.noreply.github.com>
2018-08-03 14:25:20 +1000

    Book spacing fixed
     - horizontal and vertical spacing fixed

--- src/Book.java
+++ src/Book.java
@@ -1,96 +1,80 @@
 import java.io.Serializable;
 
-
 @SuppressWarnings("serial")
 public class Book implements Serializable {
-	
-	private String title;
-	private String author;
-	private String callNo;
-	private int id;
-	
-	private enum State { AVAILABLE, ON_LOAN, DAMAGED, RESERVED };
-	private State state;
-	
-	
-	public Book(String author, String title, String callNo, int id) {
-		this.author = author;
-		this.title = title;
-		this.callNo = callNo;
-		this.id = id;
-		this.state = State.AVAILABLE;
-	}
-	
-	public String toString() {
-		StringBuilder sb = new StringBuilder();
-		sb.append("Book: ").append(id).append("\n")
-		  .append("  Title:  ").append(title).append("\n")
-		  .append("  Author: ").append(author).append("\n")
-		  .append("  CallNo: ").append(callNo).append("\n")
-		  .append("  State:  ").append(state);
-		
-		return sb.toString();
-	}
 
-	public Integer getId() {
-		return id;
-	}
+    private String title;
+    private String author;
+    private String callNo;
+    private int id;
+    
+    private enum State {AVAILABLE, ON_LOAN, DAMAGED, RESERVED};
+    private State state;
 
-	public String getTitle() {
-		return title;
-	}
+    public Book(String author, String title, String callNo, int id) {
+        this.author = author;
+        this.title = title;
+        this.callNo = callNo;
+        this.id = id;
+        this.state = State.AVAILABLE;
+    }
 
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Book: ").append(id).append("\n")
+        .append("  Title:  ").append(title).append("\n")
+        .append("  Author: ").append(author).append("\n")
+        .append("  CallNo: ").append(callNo).append("\n")
+        .append("  State:  ").append(state);
+        return sb.toString();
+    }
 
-	
-	public boolean available() {
-		return state == State.AVAILABLE;
-	}
+    public Integer getId() {
+        return id;
+    }
 
-	
-	public boolean onLoan() {
-		return state == State.ON_LOAN;
-	}
+    public String getTitle() {
+        return title;
+    }
 
-	
-	public boolean damaged() {
-		return state == State.DAMAGED;
-	}
+    public boolean available() {
+        return state == State.AVAILABLE;
+    }
 
-	
-	public void borrow() {
-		if (state.equals(State.AVAILABLE)) {
-			state = State.ON_LOAN;
-		}
-		else {
-			throw new RuntimeException(String.format("Book: cannot borrow while book is in state: %s", state));
-		}
-		
-	}
+    public boolean onLoan() {
+        return state == State.ON_LOAN;
+    }
 
+    public boolean damaged() {
+        return state == State.DAMAGED;
+    }
 
-	public void returned(boolean DAMAGED) {
-		if (state.equals(State.ON_LOAN)) {
-			if (DAMAGED) {
-				state = State.DAMAGED;
-			}
-			else {
-				state = State.AVAILABLE;
-			}
-		}
-		else {
-			throw new RuntimeException(String.format("Book: cannot Return while book is in state: %s", state));
-		}		
-	}
+    public void borrow() {
+        if (state.equals(State.AVAILABLE)) {
+          state = State.ON_LOAN;
+        } else {
+          throw new RuntimeException(String.format("Book: cannot borrow while book is in state: %s", state));
+        }
+    }
 
-	
-	public void repair() {
-		if (state.equals(State.DAMAGED)) {
-			state = State.AVAILABLE;
-		}
-		else {
-			throw new RuntimeException(String.format("Book: cannot repair while book is in state: %s", state));
-		}
-	}
+    public void returned(boolean DAMAGED) {
+        if (state.equals(State.ON_LOAN)) {
+          if (DAMAGED) {
+            state = State.DAMAGED;
+          } else {
+            state = State.AVAILABLE;
+          }
+        } else {
+          throw new RuntimeException(String.format("Book: cannot Return while book is in state: %s", state));
+        }
+    }
 
+    public void repair() {
+        if (state.equals(State.DAMAGED)) {
+          state = State.AVAILABLE;
+        } else {
+          throw new RuntimeException(String.format("Book: cannot repair while book is in state: %s", state));
+        }
+    }
 
 }
