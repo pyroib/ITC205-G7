60d484525775b7b3150161a88f8a21da9ead42cd
[twistedhead] <[ashekeenkhan@gmail.com]>
2018-08-04 13:15:28 +1000

    Fixed indentation and line breaks

--- src/Loan.java
+++ src/Loan.java
@@ -4,16 +4,17 @@ import java.util.Date;
 
 @SuppressWarnings("serial")
 public class Loan implements Serializable {
-	
-	public static enum LoanState { CURRENT, OVER_DUE, DISCHARGED };
-	
+
+	public static enum LoanState {
+		CURRENT, OVER_DUE, DISCHARGED
+	};
+
 	private int id;
 	private book book;
 	private member member;
 	private Date date;
 	private LoanState state;
 
-	
 	public Loan(int loanId, book book, member member, Date dueDate) {
 		this.id = loanId;
 		this.book = book;
@@ -22,57 +23,46 @@ public class Loan implements Serializable {
 		this.state = LoanState.CURRENT;
 	}
 
-	
 	public void checkOverDue() {
-		if (state == LoanState.CURRENT &&
-			Calendar.getInstance().Date().after(date)) {
-			this.state = LoanState.OVER_DUE;			
+		if (state == LoanState.CURRENT && Calendar.getInstance().Date().after(date)) {
+			this.state = LoanState.OVER_DUE;
 		}
 	}
 
-	
 	public boolean isOverDue() {
 		return state == LoanState.OVER_DUE;
 	}
 
-	
 	public Integer getId() {
 		return id;
 	}
 
-
 	public Date getDueDate() {
 		return date;
 	}
-	
-	
+
 	public String toString() {
 		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
 
 		StringBuilder stringBuilder = new StringBuilder();
-		stringBuilder.append("Loan:  ").append(id).append("\n")
-		  .append("  Borrower ").append(member.getId()).append(" : ")
-		  .append(member.getLastName()).append(", ").append(member.getFirstName()).append("\n")
-		  .append("  Book ").append(book.ID()).append(" : " )
-		  .append(book.Title()).append("\n")
-		  .append("  DueDate: ").append(simpleDateFormat.format(date)).append("\n")
-		  .append("  State: ").append(state);		
+		stringBuilder.append("Loan:  ").append(id).append("\n").append("  Borrower ").append(member.getId())
+				.append(" : ").append(member.getLastName()).append(", ").append(member.getFirstName()).append("\n")
+				.append("  Book ").append(book.ID()).append(" : ").append(book.Title()).append("\n")
+				.append("  DueDate: ").append(simpleDateFormat.format(date)).append("\n").append("  State: ")
+				.append(state);
 		return stringBuilder.toString();
 	}
-
 
 	public member member() {
 		return member;
 	}
 
-
 	public book book() {
 		return book;
 	}
 
-
 	public void loan() {
-		state = LoanState.DISCHARGED;		
+		state = LoanState.DISCHARGED;
 	}
 
 }
