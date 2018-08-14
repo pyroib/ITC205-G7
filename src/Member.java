23b911c0903b4d3ec48bf515c284521507e12368
Ian Blott <github@blott.com.au>
2018-08-14 10:07:35 +1000

    fixed issue ticket #24 - indentation

--- src/Member.java
+++ src/Member.java
@@ -4,127 +4,124 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-
 @SuppressWarnings("serial")
 public class Member implements Serializable {
-
-  private String lastName;
-  private String firstName;
-  private String email;
-  private int phoneNumber;
-  private int ID;
-  private double fines;
-  private Map<Integer, Loan> loans;
-
-  
-  public Member(String lastName, String firstName, String email, int phoneNumber, int ID) {
-    this.lastName = lastName;
-    this.firstName = firstName;
-    this.email = email;
-    this.phoneNumber = phoneNumber;
-    this.ID = ID;
-
-    this.loans = new HashMap<>();
-  }
-
-  
-  public String toString() {
-    StringBuilder stringBuilder = new StringBuilder();
-    stringBuilder.append("Member:  ")
-                .append(ID)
-                .append("\n")
-                .append("  Name:  ")
-                .append(lastName)
-                .append(", ")
-                .append(firstName)
-                .append("\n")
-                .append("  Email: ")
-                .append(email)
-                .append("\n")
-                .append("  Phone: ")
-                .append(phoneNumber)
-                .append("\n")
-                .append(String.format("  Fines Owed :  $%.2f", fines))
-                .append("\n");
-
-    for (Loan loan : loans.values()) {
-      stringBuilder.append(loan).append("\n");
+    
+    private String lastName;
+    private String firstName;
+    private String email;
+    private int phoneNumber;
+    private int ID;
+    private double fines;
+    private Map<Integer, Loan> loans;
+    
+    
+    public Member(String lastName, String firstName, String email, int phoneNumber, int ID) {
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.ID = ID;
+        
+        this.loans = new HashMap<>();
     }
-    return stringBuilder.toString();
-  }
-
-  
-  public int getId() {
-    return ID;
-  }
-
-  
-  public List<Loan> getLoans() {
-    List<Loan> loansList = new ArrayList<Loan>(loans.values()); 
-    return loansList;
-  }
-
-  
-  public int getNumberOfCurrentLoans() {
-    return loans.size();
-  }
-
-  
-  public double getFinesOwed() {
-    return fines;
-  }
-
-  
-  public void takeOutLoan(Loan loan) {
-    int loanId = loan.getId();
-    boolean loanExists = loans.containsKey(loanId);
-    if (!loanExists) {
-      loans.put(loanId, loan);
-    } else {
-      throw new RuntimeException("Duplicate loan added to member");
+    
+    
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("Member:  ")
+            .append(ID)
+            .append("\n")
+            .append("  Name:  ")
+            .append(lastName)
+            .append(", ")
+            .append(firstName)
+            .append("\n")
+            .append("  Email: ")
+            .append(email)
+            .append("\n")
+            .append("  Phone: ")
+            .append(phoneNumber)
+            .append("\n")
+            .append(String.format("  Fines Owed :  $%.2f", fines))
+            .append("\n");
+        for (Loan loan : loans.values()) {
+            stringBuilder.append(loan).append("\n");
+        }
+        return stringBuilder.toString();
     }
-  }
-
-  
-  public String getLastName() {
-    return lastName;
-  }
-
-  
-  public String getFirstName() {
-    return firstName;
-  }
-
-  
-  public void addFine(double fine) {
-    fines += fine;
-  }
-
-  
-  public double payFine(double amount) {
-    if (amount < 0) {
-      throw new RuntimeException("Member.payFine: amount must be positive");
+    
+    
+    public int getId() {
+        return ID;
     }
-    double change = 0;
-    if (amount > fines) {
-      change = amount - fines;
-      fines = 0;
-    } else {
-      fines -= amount;
+    
+    
+    public List<Loan> getLoans() {
+        List<Loan> loansList = new ArrayList<Loan>(loans.values());
+        return loansList;
     }
-    return change;
-  }
-
-  
-  public void dischargeLoan(Loan loan) {
-    int loanId = loan.getId();
-    boolean loanExists = loans.containsKey(loanId);
-    if (loanExists) {
-      loans.remove(loanId);
-    } else {
-      throw new RuntimeException("No such loan held by member");
+    
+    
+    public int getNumberOfCurrentLoans() {
+        return loans.size();
     }
-  }
-
-  
+    
+    
+    public double getFinesOwed() {
+        return fines;
+    }
+    
+    
+    public void takeOutLoan(Loan loan) {
+        int loanId = loan.getId();
+        boolean loanExists = loans.containsKey(loanId);
+        if (!loanExists) {
+            loans.put(loanId, loan);
+        } else {
+            throw new RuntimeException("Duplicate loan added to member");
+        }
+    }
+    
+    
+    public String getLastName() {
+        return lastName;
+    }
+    
+    
+    public String getFirstName() {
+        return firstName;
+    }
+    
+    
+    public void addFine(double fine) {
+        fines += fine;
+    }
+    
+    
+    public double payFine(double amount) {
+        if (amount < 0) {
+            throw new RuntimeException("Member.payFine: amount must be positive");
+        }
+        double change = 0;
+        if (amount > fines) {
+            change = amount - fines;
+            fines = 0;
+        } else {
+            fines -= amount;
+        }
+        return change;
+    }
+    
+    
+    public void dischargeLoan(Loan loan) {
+        int loanId = loan.getId();
+        boolean loanExists = loans.containsKey(loanId);
+        if (loanExists) {
+            loans.remove(loanId);
+        } else {
+            throw new RuntimeException("No such loan held by member");
+        }
+    }
+    
 }
