320ae3064cb297d5b91c7de83dd17ec79b4defb2
jazzie-b <33510723+jazzie-b@users.noreply.github.com>
2018-08-15 22:51:21 +1000

    Changes made to Calendar
    All changes made as per issue #36, except, line 54. This is due to the variable 'now' being declared only 3 lines before being used, which I believe to be within reason. 4.8.2.2 states "local variables are declared close to the point they are first used (within reason)"

--- src/Calendar.java
+++ src/Calendar.java
@@ -13,7 +13,7 @@ public class Calendar {
     
     public static Calendar getInstance() {
         if (self == null) {
-          self = new Calendar();
+            self = new Calendar();
         }
         return self;
     }
@@ -26,26 +26,26 @@ public class Calendar {
     
     public synchronized void setDate(Date date) {
         try {
-          calendar.setTime(date);
-          calendar.set(java.util.Calendar.HOUR_OF_DAY, 0);
-          calendar.set(java.util.Calendar.MINUTE, 0);
-          calendar.set(java.util.Calendar.SECOND, 0);
-          calendar.set(java.util.Calendar.MILLISECOND, 0);
-        } catch (Exception e) {
-          throw new RuntimeException(e);
+            calendar.setTime(date);
+            calendar.set(java.util.Calendar.HOUR_OF_DAY, 0);
+            calendar.set(java.util.Calendar.MINUTE, 0);
+            calendar.set(java.util.Calendar.SECOND, 0);
+            calendar.set(java.util.Calendar.MILLISECOND, 0);
+        } catch (Exception expected1) {
+            throw new RuntimeException(expected1);
         }
     }
 
     
     public synchronized Date setDate() {
         try {
-          calendar.set(java.util.Calendar.HOUR_OF_DAY, 0);
-          calendar.set(java.util.Calendar.MINUTE, 0);
-          calendar.set(java.util.Calendar.SECOND, 0);
-          calendar.set(java.util.Calendar.MILLISECOND, 0);
-          return calendar.getTime();
-        } catch (Exception e) {
-          throw new RuntimeException(e);
+            calendar.set(java.util.Calendar.HOUR_OF_DAY, 0);
+            calendar.set(java.util.Calendar.MINUTE, 0);
+            calendar.set(java.util.Calendar.SECOND, 0);
+            calendar.set(java.util.Calendar.MILLISECOND, 0);
+            return calendar.getTime();
+        } catch (Exception expected2) {
+            throw new RuntimeException(expected2);
         }
     }
 
