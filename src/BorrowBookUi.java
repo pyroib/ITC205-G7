ef31379759e490a599e9df94a649d28672b628d8
jazzie-b <33510723+jazzie-b@users.noreply.github.com>
2018-08-15 23:16:32 +1000

    BorrowBookUi changes made
    All changes made to BorrowBookUi as requested by issue #35. Except, methods modifier is being left as private.

--- src/BorrowBookUi.java
+++ src/BorrowBookUi.java
@@ -2,14 +2,15 @@ import java.util.Scanner;
 
 public class BorrowBookUi {
 
+	
     public static enum UiState {
         INITIALISED, READY, RESTRICTED, SCANNING, IDENTIFIED, FINALISING, COMPLETED, CANCELLED
     };
-
     private BorrowBookControl control;
     private Scanner input;
     private UiState state;
 
+    
     public BorrowBookUi(BorrowBookControl control) {
         this.control = control;
         input = new Scanner(System.in);
@@ -17,77 +18,87 @@ public class BorrowBookUi {
         control.setUi(this);
     }
 
-    private String getInput(String prompt) {
-        System.out.print(prompt);
-        return input.nextLine();
-    }
-
-    private void printOutput(Object object) {
-        System.out.println(object);
-    }
-
+    
     public void setState(UiState state) {
         this.state = state;
     }
 
+    
     public void run() {
         printOutput("Borrow Book Use Case UI\n");
         while (true) {
-          switch (state) {
-            case CANCELLED:
-              printOutput("Borrowing Cancelled");
-              return;
-            case READY:
-              String memberString = getInput("Swipe member card (press <enter> to cancel): ");
-              if (memberString.length() == 0) {
-                control.cancel();
-                break;
-              }
-              try {
-                int memberId = Integer.valueOf(memberString).intValue();
-                control.swiped(memberId);
-              } catch (NumberFormatException e) {
-                printOutput("Invalid Member Id");
-              }
-              break;
+            switch (state) {
+                case CANCELLED:
+                    printOutput("Borrowing Cancelled");
+                    return;
+                case READY:
+                    String memberString = getInput("Swipe member card (press <enter> to cancel): ");
+                    int memberLength = memberString.length();
+                    if (memberLength == 0) {
+                        control.cancel();
+                        break;
+                    }
+                    try {
+                        int memberId = Integer.valueOf(memberString).intValue();
+                        control.swiped(memberId);
+                    } catch (NumberFormatException excepted1) {
+                        printOutput("Invalid Member Id");
+                    }
+                    break;
             case RESTRICTED:
-              getInput("Press <any key> to cancel");
-              control.cancel();
-              break;
-            case SCANNING:
-              String bookString = getInput("Scan Book (<enter> completes): ");
-              if (bookString.length() == 0) {
-                control.complete();
-                break;
-              }
-              try {
-                int bookId = Integer.valueOf(bookString).intValue();
-                control.scanned(bookId);
-              } catch (NumberFormatException e) {
-                printOutput("Invalid Book Id");
-              }
-              break;
-            case FINALISING:
-              String answer = getInput("Commit loans? (Y/N): ");
-              if (answer.toUpperCase().equals("N")) {
+                getInput("Press <any key> to cancel");
                 control.cancel();
-              } else {
-                control.commitLoans();
-                getInput("Press <any key> to complete ");
-              }
-              break;
+                break;
+            case SCANNING:
+                String bookString = getInput("Scan Book (<enter> completes): ");
+                int bookLength = bookString.length();
+                if (bookLength == 0) {
+                    control.complete();
+                    break;
+                }
+                try {
+                    int bookId = Integer.valueOf(bookString).intValue();
+                    control.scanned(bookId);
+                } catch (NumberFormatException excepted2) {
+                    printOutput("Invalid Book Id");
+                }
+                break;
+            case FINALISING:
+                String answer = getInput("Commit loans? (Y/N): ");
+                boolean answerNo = answer.toUpperCase().equals("N");
+                if (answerNo) {
+                    control.cancel();
+                } else {
+                    control.commitLoans();
+                    getInput("Press <any key> to complete ");
+                }
+                break;
             case COMPLETED:
-              printOutput("Borrowing Completed");
-              return;
+                printOutput("Borrowing Completed");
+                return;
             default:
-              printOutput("Unhandled state");
-              throw new RuntimeException("BorrowBookUi : unhandled state :" + state);
-          }
+                printOutput("Unhandled state");
+                throw new RuntimeException("BorrowBookUi : unhandled state :" + state);
+            }
         }
     }
-
-    public void display(Object object) {
+    
+    
+    public void displayOutput(Object object) {
         printOutput(object);
     }
+    
+    
+    private String getInput(String prompt) {
+        System.out.print(prompt);
+        String userInput = input.nextLine();
+        return userInput;
+    }
 
+    
+    private void printOutput(Object object) {
+        System.out.println(object);
+    }
+
+    
 }
